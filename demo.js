2cb8aec6ea5d7eec6af5931c3f71d4bf17f93030
patrick <patrick@knowhere.guru>
2014-10-05 12:54:55 +0200

    Imgur Demo Page

---
+++ demo.js
@@ -0,0 +1,30 @@
+'use strict';
+
+angular
+  .module('demoApp', [
+    'pz101.ztiles'
+]).controller('DemoCtrl', function($scope, $http) {
+	$http.get('https://api.imgur.com/3/gallery/hot/viral/0.json', {headers : {Authorization : 'Client-ID 09b0999bc7f8c9e'}}).success(function(html) {
+		var data = html.data;
+		var i = 0;
+		$scope.tiles = data.filter(function(entry) {
+			i++;
+			return angular.isDefined(entry.width) && i < 2000;
+		});
+		
+	}).error(function(e) {
+		console.log(e);
+	});
+}).directive('fadeing', function() {
+	return {
+		restrict: 'A',
+		link: function(scope, element) {
+			element.bind('mouseenter', function() {
+				element.find('.fadeing').fadeIn('slow');
+			});
+			element.bind('mouseleave', function() {
+				element.find('.fadeing').hide();
+			});
+		}
+    };
+});

725e1d3889fc52dd9907b117fba166035037b297
patrick <patrick@knowhere.guru>
2014-10-11 13:18:27 +0200

    update readme and grunt build

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,68 +13,69 @@
 
 'use strict';
 
-module.exports = function (grunt) {
+module.exports = function(grunt) {
+  
+  require('load-grunt-tasks')(grunt);
 
-    var banner = '/*! <%= pkg.name %> (v<%= pkg.version %>) - Copyright: 2014, <%= pkg.author %> - <%= pkg.license %> */\n';
+  var banner = '/*! <%= pkg.name %> (v<%= pkg.version %>) - ' +
+    'Copyright: 2014, <%= pkg.author %> - <%= pkg.license %> */\n';
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('bower.json'),
-        uglify: {
-            options: {
-                preserveComments: 'some',
-                report: 'gzip',
-                mangle: false,
-                compress: {
-                     drop_console: true
-                }
-            },
-            dist: {
-                src: '<%= pkg.name %>.js',
-                dest: '<%= pkg.name %>.min.js'
-            }
-        },
-        concat: {
-            options: {
-                banner: banner
-            },
-            dist: {
-                src: ['src/ztiles.js'],
-                dest: '<%= pkg.name %>.js'
-            }
-        },
-        jshint: {
-            options: {
-                jshintrc: '.jshintrc',
-                reporter: require('jshint-stylish')
-            },
-            all: [
-                'src/*.js',
-                'example/*.js'
-            ]
-        },
-        jscs: {
-            src: [
-                'src/*.js',
-                'example/*.js'
-            ],
-            options: {
-                config: '.jscsrc',
-                reporter: require( 'jscs-stylish' ).path
-            }
-        },
-        ngmin: {
-            dist: {
-                src: '<%= pkg.name %>.js',
-                dest: '<%= pkg.name %>.js'
-            }
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('bower.json'),
+    uglify: {
+      options: {
+        preserveComments: 'some',
+        report: 'gzip',
+        mangle: false,
+        compress: {
+          drop_console: true,
+          angular : true
         }
-    });
+      },
+      dist: {
+        src: '<%= pkg.name %>.js',
+        dest: '<%= pkg.name %>.min.js'
+      }
+    },
+    concat: {
+      options: {
+        banner: banner
+      },
+      dist: {
+        src: ['src/ztiles.js'],
+        dest: '<%= pkg.name %>.js'
+      }
+    },
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      },
+      all: [
+        //'Gruntfile.js',
+        'src/*.js',
+        'example/*.js'
+      ]
+    },
+    jscs: {
+      src: [
+        //'Gruntfile.js',
+        'src/*.js',
+        'example/*.js'
+      ],
+      options: {
+        config: '.jscsrc',
+        reporter: require('jscs-stylish').path
+      }
+    },
+    ngmin: {
+      dist: {
+        src: '<%= pkg.name %>.js',
+        dest: '<%= pkg.name %>.js'
+      }
+    }
+  });
 
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks("grunt-jscs");
-    
-    grunt.registerTask('test', ['jshint', 'jscs']);
-    grunt.registerTask('default', ['test', 'concat', 'uglify']);
+  grunt.registerTask('test', ['jshint', 'jscs']);
+  grunt.registerTask('default', ['test', 'concat', 'uglify']);
 };
--- README.md
+++ README.md
@@ -4,10 +4,17 @@ angular-ztiles
 Self adaptive tiles in AngularJS like www.yahoo.com/food.
 
 Demo: http://patrickzimmermann101.github.io/angular-ztiles
-
 Demo Screenshot:
 
 ![Demo Screenshot](https://raw.github.com/patrickzimmermann101/angular-ztiles/master/demo.jpg)
+
+## Technique
+
+Example of a layout with four tiles in a row. 
+
+![Layout](https://raw.github.com/patrickzimmermann101/angular-ztiles/master/layout.jpg)
+
+The algorithm has two alignment modes `left`and `right`. The arrangement is always separating a row in an element from `tile-type=c` spanning over two sub rows. In alignment mode `left`this element is on the left. The others are building two sub rows (`tile-type=a`and `tile-type=b`). Each tile has an margin of the padding value (Default is 4px).
 
 ## Installation
 
@@ -20,7 +27,82 @@ Demo Screenshot:
 
 3. Inject the `angular-ztiles` module in your application.
 
-    ```js
+   ```js
     angular.module('your.module', [
         'pz101.ztiles'
     ]);
+ ```
+
+## How To
+
+### Minimal use as a directive in your HTML
+
+1. You need an array of elements which represents your tiles. Each element need a  width and height key.
+2. Mark your tiles section with the attribute `z-tiles`.
+3. Bind your array to the element. `tiles=[...]`.
+	
+	```html
+    <div z-tiles tiles="tiles">
+	    <p>Your Tile</p>
+    </div>
+    ```
+Example for a controller class
+
+	```js
+	...
+	$scope.tiles = [{
+		width: 200,
+		height: 200,
+		body: 'Hello Tile A'
+	},{
+		width: 300,
+		height: 100,
+		body: 'Hello Tile B'
+	},{
+		width: 150,
+		height: 300,
+		body: 'Hello Tile C'}];
+	...
+	```
+4. You can now access to all data of an element in your array inside your tile definition.
+
+	```html
+    <div z-tiles tiles="tiles">
+	    <p>{{tile.body}}</p>
+    </div>
+   ```
+
+5. Example for accessing the mother scope of the controller.
+
+	```html
+    <div z-tiles tiles="tiles">
+	    <p>{{tile.body}}</p>
+	    <button ng-click="mother.click(tile)">Click</button>
+    </div>
+   ```
+```js
+	...
+	$scope.click = function(tile) {
+		console.log('Clicked Button: ' + tile.body);
+	};
+	
+	$scope.tiles = [{
+		width: 200,
+		height: 200,
+		body: 'Hello Tile A'
+	},{
+		width: 300,
+		height: 100,
+		body: 'Hello Tile B'
+	},{
+		width: 150,
+		height: 300,
+		body: 'Hello Tile C'}];
+	...
+	```
+
+### Options
+
+#### Padding
+
+tbd

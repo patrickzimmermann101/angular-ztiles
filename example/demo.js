6301fae6be8cc8d7723ef202861f70176d7a5691
patrick <patrick@knowhere.guru>
2014-10-24 15:05:26 +0200

    update demo

--- example/demo.js
+++ example/demo.js
@@ -18,13 +18,19 @@ angular.module('demoApp', ['pz101.ztiles']).controller('DemoCtrl',
     $http.get('https://api.imgur.com/3/gallery/t/earthporn/top/0.json',
       {headers: {Authorization: 'Client-ID 09b0999bc7f8c9e'}}).
         success(function(html) {
-
           var data = html.data,
-            i = 0;
+            i = 0, tiles;
 
-          $scope.tiles = data.items.filter(function(entry) {
+          tiles = data.items.filter(function(entry) {
             i++;
-            return angular.isDefined(entry.width) && i < 30;
+            return angular.isDefined(entry.width) && i < 50 && i > 10;
+          });
+
+          $scope.tiles = tiles.map(function(entry) {
+            var lastPoint = entry.link.lastIndexOf('.');
+            entry.thumb = entry.link.substring(0, lastPoint) + 'l' +
+              entry.link.substring(lastPoint, entry.link.length);
+            return entry;
           });
 
         }).error(function(e) {
